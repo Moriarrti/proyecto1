ba7c466f6c0dec1f9f7490aa9315b9840023f6f1
Jose <jrmonjepadilla@gmailcom>
2022-01-24 09:28:18 +0100

    rama cambio

---
+++ prueba1/src/cambio1/OtroCambio.java
@@ -0,0 +1,8 @@
+package cambio1;
+
+public class OtroCambio {
+	public static void main(String[] args) {
+		System.out.println("Hola");
+	}
+
+}
--- prueba1/src/prueba1/prueba1.java
+++ prueba1/src/prueba1/prueba1.java
@@ -3,11 +3,12 @@ package prueba1;
 public class prueba1 {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java-git");
-		//añado comentario
-		System.out.println();
-		System.out.println("Java git");
-		System.out.println("dnsodi");
+		System.out.println("Hola mundo java-git");
+		System.out.println("Adios");
+		System.out.println("Pruebas Git. Se añade Cambio1");
+
 	}
+
 
 
 }
